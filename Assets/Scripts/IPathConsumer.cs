7ee05909c70fec5165896546c71daac5a8af968a
Gadwicke <Gadriel@gmail.com>
2014-09-01 13:03:30 -0600

    Blocked out PathController and IPathConsumer.

---
+++ Assets/Scripts/IPathConsumer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IPathConsumer {
+
+    //Navigation properties
+    float MaximumSpeed { get; }
+    float Acceleration { get; }
+    float Deceleration { get; }
+    float TurnRate { get; }
+    float CurrentSpeed { get; }
+}
--- Assets/Scripts/PathController.cs
+++ Assets/Scripts/PathController.cs
@@ -1,15 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
+public class Path
+{
+    public IPathConsumer Actor;
+    private Queue<Vector2> Points = new Queue<Vector2>(30);
+
+    public Vector2 GetNext()
+    {
+        return Points.Peek();
+    }
+
+    public Vector2 Consume()
+    {
+        return Points.Dequeue();
+    }
+}
 
 public class PathController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private List<Path> Paths = new List<Path>();
+
+    public void Update()
+    {
+        for (int i = 0; i < Paths.Count; i++)
+        {
+            MoveAlong(Paths[i]);
+        }
+    }
+
+    private void MoveAlong(Path path)
+    {
+        //Move the Actor for the path toward the next point if one exists
+        //Accelerate/decelerate as appropriate
+    }
 }
